eeb0b45eebf05dceb6e1afb8f5f48c9283679f78
shreyakulkarni21 <shreyakulkarni2112@gmail.com>
2024-04-06 16:03:07 +0530

    new code

--- web/browser/screens/SearchPage.js
+++ web/browser/screens/SearchPage.js
@@ -1,20 +1,25 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, FlatList, Text } from 'react-native';
+import React, { useState,useEffect } from 'react';
+import { View, TextInput, Button, FlatList, Text,SafeAreaView, RefreshControlBase } from 'react-native';
 import axios from 'axios';
 
 const SearchPage = () => {
-  const [searchQuery, setSearchQuery] = useState('tea'); // Set default search query to 'tea'
+  const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  useEffect(() => {
+    const searchQuery = "tea"; 
+    handleSearch(searchQuery);
+  }, []);
 
   const handleSearch = () => {
+    //const searchQuery = "tea"; 
     axios.get('http://10.105.24.34:8000/server/get_web_results/', {
       params: {
-        search_query: searchQuery // Use the state value for search query
+        search_query: searchQuery
       }
     })
     .then(function (response) {
       console.log(response);
-      setSearchResults(response.data.results);
+      //setSearchResults(response.data.results);
     })
     .catch(function (error) {
       console.log(error);
@@ -25,21 +30,18 @@ const SearchPage = () => {
   };
 
   return (
-    <View style={{ flex: 1, padding: 10 }}>
-      <TextInput
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10, paddingHorizontal: 10 }}
-        placeholder="Enter search query"
-        onChangeText={text => setSearchQuery(text)}
-        value={searchQuery}
-      />
-      <Button title="Search" onPress={handleSearch} />
+    <SafeAreaView style={{ flex: 1, padding: 10,marginTop:50, }}>
+      
+      
+      <Button title="Search" onPress={() => handleSearch("tea")} />
       <FlatList
         data={searchResults}
         keyExtractor={(item, index) => index.toString()}
         renderItem={({ item }) => <Text>{item.title}</Text>}
       />
-    </View>
+    </SafeAreaView>
   );
 };
+
 
 export default SearchPage;
